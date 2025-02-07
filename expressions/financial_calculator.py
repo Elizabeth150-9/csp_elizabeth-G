1d734bce2f3021ca157f52160aabb25d8903d2c0
Elizabeth.105 <elizabeth.gutierrez@ucas-edu.net>
2025-02-07 11:32:35 -0700

    practice

---
+++ expressions/financial_calculator.py
@@ -0,0 +1,39 @@
+# Elizabeth Gutierrez, Financial Calculator python
+
+# print statement that welcomes user and tells what the program does
+
+# ask what their income is (variable an input)
+income = float(input("What is your income\n"))
+# ask what their rent is (variable an input)
+rent = float(input("What is your rent\n"))
+# ask what their utilities is (variable an input)
+utilities = float(input("What are your utilities\n"))
+# ask what their groceries is (variable an input)
+groceries = float(input("What are your groceries\n"))
+# ask what their transportation is (variable an input)
+transportation = float(input("What is your transportation\n"))
+# calculate savings as 10% of income (income*.1) (variable)
+savings = float(input("What are your savings\n"))
+# calculate spending as income-savings-rent-utilities-groceries-transportation (variable)
+spending = float(input("What are your spendings\n"))
+# calculate percent income of rent (rent/income *100) (variable)
+
+# calculate percent income of utilities (utilities/income *100) (variable)
+
+# calculate percent income of groceries (groceries/income *100) (variable)
+
+# calculate percent income of transportation (transportation/income *100) (variable)
+
+# calculate percent income of spending (spending/income *100) (variable)
+
+# Your rent is $XX.XX which is XX% of your income. (Print)
+
+# Your utilities is $XX.XX which is XX% of your income. (Print)
+
+# Your groceries is $XX.XX which is XX% of your income. (Print)
+
+# Your transportation is $XX.XX which is XX% of your income. (Print)
+
+# Your savings is $XX.XX which is XX% of your income. (Print)
+
+# Your spending is $XX.XX which is XX% of your income. (Print)
--- expressions/practice.py
+++ expressions/practice.py
@@ -3,49 +3,9 @@
 print(7-24/8*4+6)
 print(18/3-7+2*5)
 print(6*4/12+72/8-9)
-print(17-6/2*+4*3)
-print(-2*(1*4-2/2)*+(6+2-3))
-print(-1*(5**/15)*-(5-2**))
-print(1**4*2+3*-2**5/4)
-print(22/2-2*5)**+(4-6/6)**
-
-num = 1
-
-#print(num)
-
-num = 9
-
-#print(num)
-
-num = 2
-
-#print(num)
-
-num = 26
-
-#print(num)
-
-num = -1
-
-#print(num)
-
-num = -3
-
-#print(num)
-
-num = 4
-
-#print(num)
-
-num = 23
-
-#print(num)
-
-num = 10
-
-#print(num)
-
-print(f"{pi:.4f}")
-
-
-
+print((17-6/2)+4*3)
+print(-2*(1*4-2/2)+(6+2-3))
+print(-1*((3-4*7)5)-2*24/6)
+print((3*5**2/15)-(5-2**2))
+print((1*2*+3*)-2*/4)
+print((22/2-2.5)+(4-6/6)2)
