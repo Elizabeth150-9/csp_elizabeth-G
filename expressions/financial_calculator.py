4616ccb5232f7c466774d018bc9c7fb51bcff674
Elizabeth150-9 <elizabeth.gutierrez@ucas-edu.net>
2025-02-11 03:00:38 +0000

    okls

--- expressions/financial_calculator.py
+++ expressions/financial_calculator.py
@@ -5,11 +5,11 @@
 # ask what their income is (variable an input)
 income = float(input("What is your income\n"))
 # ask what their rent is (variable an input)
-rent = float(input("What is your rent\n"))
+rent = float(input("How much is your rent\n"))
 # ask what their utilities is (variable an input)
 utilities = float(input("What are your utilities\n"))
 # ask what their groceries is (variable an input)
-groceries = float(input("What are your groceries\n"))
+groceries = float(input("How much are your groceries\n"))
 # ask what their transportation is (variable an input)
 transportation = float(input("What is your transportation\n"))
 # calculate savings as 10% of income (income*.1) (variable)
@@ -17,15 +17,15 @@ savings = float(input("What are your savings\n"))
 # calculate spending as income-savings-rent-utilities-groceries-transportation (variable)
 spending = float(input("What are your spendings\n"))
 # calculate percent income of rent (rent/income *100) (variable)
-
+printf = ("")
 # calculate percent income of utilities (utilities/income *100) (variable)
-
+printf = ("")
 # calculate percent income of groceries (groceries/income *100) (variable)
-
+printf = ("")
 # calculate percent income of transportation (transportation/income *100) (variable)
-
+printf = ("")
 # calculate percent income of spending (spending/income *100) (variable)
-
+printf = ("")
 # Your rent is $XX.XX which is XX% of your income. (Print)
 
 # Your utilities is $XX.XX which is XX% of your income. (Print)
