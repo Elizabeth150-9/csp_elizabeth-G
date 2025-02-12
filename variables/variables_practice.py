76d211f97c1b8c9633e1931ef5d1f939dbefba49
Elizabeth.105 <elizabeth.gutierrez@ucas-edu.net>
2025-02-12 11:27:42 -0700

    vp

---
+++ variables/variables_practice.py
@@ -0,0 +1,20 @@
+# Elizabeth Gutierrez, variables practice python
+
+name = input("Elizabeth")
+
+small = input(2)
+
+number = input(52)
+
+breakfast = input("shake")
+
+color = input("Green")
+
+school = input("UCAS")
+
+year = input(2025)
+
+age = input(15)
+
+subject = input("Biology")
+
