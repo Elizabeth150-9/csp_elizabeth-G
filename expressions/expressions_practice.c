cf8e49f6ca3f29247ed1814fc49a124bccc5b860
Elizabeth150-9 <elizabeth.gutierrez@ucas-edu.net>
2025-02-13 19:20:01 +0000

    klj

--- expressions/expressions_practice.c
+++ expressions/expressions_practice.c
@@ -2,24 +2,24 @@
 #include <stdio.h>
 #include <math.h>
 int main(void){
-float equation = 7-24/8*4+6;
-printf("%s\n", equation);
-float big_equation = 18/3-7+2*5;
-printf("%s\n", big_equation);
-float bigger_equation = 6*4/12+72/8-9;
-printf("%s\n", bigger_equation);
-float bigg_equation = 17-6/2+4*3;
-printf("%s\n", bigg_equation);
-float bigerr_equation = -2*(1*4-2/2)+6+2-3;
-printf("%s\n", bigerr_equation);
-float  biig_equation = -1*(3-4*7)/5-2*24/6;
-printf("%s\n", biig_equation);
-float longer_equation = (3*(pow)(5, 2)/15)-(5-(pow)(2, 2));
-printf("%s\n", longer_equation);
-float longest_equation = (1*(pow)(1, 4))(2*(pow)(2, 2))+(3(pow)(3, 3))(-2(pow)(22, 5))/4;
-printf("%s\n", longest_equation);
-float wide_equation = 22/2-(2.5*(pow)(2.5, 2))+4-6(6(pow)(6, 2));
-printf("%s\n", wide_equation);
+int equation = 7-24/8*4+6;
+printf("%d\n", equation);
+int big_equation = 18/3-7+2*5;
+printf("%d\n", big_equation);
+int bigger_equation = 6*4/12+72/8-9;
+printf("%d\n", bigger_equation);
+int bigg_equation = 17-6/2+4*3;
+printf("%d\n", bigg_equation);
+int bigerr_equation = -2*(1*4-2/2)+6+2-3;
+printf("%d\n", bigerr_equation);
+int  biig_equation = -1*(3-4*7)/5-2*24/6;
+printf("%d\n", biig_equation);
+int longer_equation = (3*(pow)(5, 2)/15)-(5-(pow)(2, 2));
+printf("%d\n", longer_equation);
+int longest_equation = (1*(pow)(1, 4))(2*(pow)(2, 2))+(3(pow)(3, 3))(-2(pow)(22, 5))/4;
+printf("%d\n", longest_equation);
+int wide_equation = 22/2-(2.5*(pow)(2.5, 2))+4-6(6(pow)(6, 2));
+printf("%d\n", wide_equation);
  return 0;
 }
 
