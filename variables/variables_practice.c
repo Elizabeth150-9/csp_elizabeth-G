44ff8a6c449d3bd43743e6f1ad084b3b68830f10
Elizabeth150-9 <elizabeth.gutierrez@ucas-edu.net>
2025-02-13 04:38:30 +0000

    variables_practice

---
+++ variables/variables_practice.c
@@ -0,0 +1,36 @@
+// Elizabeth Gutierrez, Variables Practice C
+#include<stdio.h>
+
+int main(void){
+      char name [] = "Elizabeth";
+      printf(" %s,", name);
+
+      char number [] = "2";
+      printf(" %s,", number);
+
+      char nmbr [] = "52";
+      printf(" %s,", nmbr);
+
+      char breakfast [] = "shake";
+      printf(" %s,", breakfast);
+
+      char color [] = "green";
+      printf(" %s,", color);
+
+      char school [] = "UCAS";
+      printf(" %s,", school);
+
+      char year [] = "2025";
+      printf(" %s,", year);
+
+      char eyes [] = "Green";
+      printf(" %s,", eyes);
+
+      char age [] = "15";
+      printf(" %s,", age);
+
+      char subject [] = "biology";
+      printf(" %s,", subject);
+    return 0;
+}
+
