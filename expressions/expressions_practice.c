1e12905e6737a4e35abc871b249d521f909e9d0f
Elizabeth150-9 <elizabeth.gutierrez@ucas-edu.net>
2025-02-14 02:45:05 +0000

    epc

--- expressions/expressions_practice.c
+++ expressions/expressions_practice.c
@@ -2,24 +2,42 @@
 #include <stdio.h>
 #include <math.h>
 int main(void){
+
 int equation = 7-24/8*4+6;
+
 printf("%d\n", equation);
+
 int big_equation = 18/3-7+2*5;
+
 printf("%d\n", big_equation);
+
 int bigger_equation = 6*4/12+72/8-9;
+
 printf("%d\n", bigger_equation);
-int bigg_equation = 17-6/2+4*3;
+
+int bigg_equation = ((17-6/2)+4*3);
+
 printf("%d\n", bigg_equation);
-int bigerr_equation = -2*(1*4-2/2)+6+2-3;
+
+int bigerr_equation = -2*(1*4-2/2)+(6+2-3);
+
 printf("%d\n", bigerr_equation);
-int  biig_equation = -1*(3-4*7)/5-2*24/6;
+
+int  biig_equation = -1*((3-4*7)/5)-2*24/6;
+
 printf("%d\n", biig_equation);
-int longer_equation = (3*(pow)(5, 2)/15)-(5-(pow)(2, 2));
+
+int longer_equation = (3*pow(5, 2)/15)-(5-pow(2, 2));
+
 printf("%d\n", longer_equation);
-int longest_equation = (1*(pow)(1, 4))(2*(pow)(2, 2))+(3(pow)(3, 3))(-2(pow)(22, 5))/4;
+
+int longest_equation = (pow(1, 4)*pow(2, 2)+pow(3, 3))-(pow(2, 5)/4);
+
 printf("%d\n", longest_equation);
-int wide_equation = 22/2-(2.5*(pow)(2.5, 2))+4-6(6(pow)(6, 2));
+
+int wide_equation = (pow(22/2-2*5,2)+pow(4-6/6,2));
 printf("%d\n", wide_equation);
- return 0;
+
+return 0;
 }
 
