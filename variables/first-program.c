37dac556857e6147fdf6b9f6a282d8846ef867c4
Elizabeth.105 <elizabeth.gutierrez@ucas-edu.net>
2025-02-14 11:23:15 -0700

    fpc

---
+++ variables/first-program.c
@@ -0,0 +1,12 @@
+// Elizabeth Gutierrez, First Program C
+#include <stdio.h>
+
+char hobby;
+
+int main(void){
+    printf("Elizabeth like to play the guitar \n");
+     scanf("%s\n", hobby);
+    
+    return 0;
+
+}
