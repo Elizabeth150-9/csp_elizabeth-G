4ff557712d75e0c7c963effc98faed8bf37495a0
Elizabeth150-9 <elizabeth.gutierrez@ucas-edu.net>
2025-02-15 23:46:52 +0000

    sdf

--- strings/silly_sentences.c
+++ strings/silly_sentences.c
@@ -1,1 +1,13 @@
 // Elizabeth Gutierrez, silly sentences C
+#include <stdio.h>
+#include <string.h>
+
+char subject[99];
+
+int main(){
+    printf("Welcome to my silly sentences program in C. \n");
+   scanf("%s", subject);
+   std::string str = "";
+   if (str.empty())
+   return 0;
+   }
