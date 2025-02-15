cc3b45d9c05b8205e8d541d7060bef5d23be3d0b
Elizabeth150-9 <elizabeth.gutierrez@ucas-edu.net>
2025-02-15 01:15:04 +0000

    fcp

--- variables/first-program.c
+++ variables/first-program.c
@@ -1,10 +1,12 @@
 // Elizabeth Gutierrez, First Program C
 #include <stdio.h>
 
+
 char hobby;
 
+
 int main(void){
-    printf("Elizabeth like to play the guitar \n");
+    printf("What is your name? \n");
      scanf("%s\n", hobby);
     
     return 0;
