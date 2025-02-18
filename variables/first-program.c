9b83b17dbe918d9c0aed9a9177722270648c19f6
Elizabeth.105 <elizabeth.gutierrez@ucas-edu.net>
2025-02-18 11:02:27 -0700

    hjk

--- variables/first-program.c
+++ variables/first-program.c
@@ -1,11 +1,14 @@
 // Elizabeth Gutierrez, First Program C
 #include <stdio.h>
 
+char name;
 char hobby;
 
 int main(void){
-    printf("Elizabeth like to play the guitar \n");
-     scanf("%s\n", hobby);
+    printf("What is your name? \n");
+    scanf("%s\n", name);
+    printf(" likes to play the guitar \n");
+    scanf("%s\n", hobby);
     
     return 0;
 
