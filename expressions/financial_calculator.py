b4d78f0eb34264bf467ae09df9e7a8df554a300d
Elizabeth.105 <elizabeth.gutierrez@ucas-edu.net>
2025-02-19 11:40:06 -0700

    gjf

--- expressions/financial_calculator.py
+++ expressions/financial_calculator.py
@@ -27,16 +27,17 @@ percent_groceries = groceries/income *100
 percent_transportation = transportation/income *100
 # calculate percent income of spending (spending/income *100) (variable)
 percent_spending = spending/income *100
+# calculate 
 percent_savings = 10%income
 # Your rent is $XX.XX which is XX% of your income. (Print)
-print("Your rent costs", rent, "which is", percent_rent, "% of your income:\n")
+print(f"Your rent costs {rent} which is {percent_rent: .2f} % of your income.\n")
 # Your utilities is $XX.XX which is XX% of your income. (Print)
-print("Your utilities cost", utilities, "which is", percent_utilities, "% of your income:\n")
+print(f"Your utilities cost {utilities} which is {percent_utilities: .2f} % of your income:\n")
 # Your groceries is $XX.XX which is XX% of your income. (Print)
-print("Your groceries cost", groceries, "which is", percent_groceries, "% of your income:\n")
+print(f"Your groceries cost {groceries} which is {percent_groceries: .2f} % of your income:\n")
 # Your transportation is $XX.XX which is XX% of your income. (Print)
-print("Your transportation costs", transportation, "which is", percent_transportation, "% of your income:\n")
+print(f"Your transportation costs {transportation} which is {percent_transportation: .2f} % of your income:\n")
 # Your savings are $XX.XX which is XX% of your income. (Print)
-print("Your savings are", savings, "which is", percent_savings, "% of your income:\n")
+print(f"Your savings are {savings} which is {percent_savings: .2f} % of your income:\n")
 # Your spending is $XX.XX which is XX% of your income. (Print)
-print("Your spendings are", spending, "which is", percent_spending, "% of your income:\n")
+print(f"Your spendings left are {spending} which is {percent_spending: .2f} % of your income:\n")
