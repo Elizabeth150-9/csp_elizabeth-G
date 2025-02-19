a823e9ce2a3b0b3162f63bf3466b78dda4f68986
Elizabeth150-9 <elizabeth.gutierrez@ucas-edu.net>
2025-02-19 00:48:32 +0000

    sdf

--- variables/first-program.c
+++ variables/first-program.c
@@ -1,11 +1,14 @@
 // Elizabeth Gutierrez, First Program c
 #include <stdio.h>
 
-char name[90];
+char name[20];
+char food[20];
 
 int main(void){
     printf("Welcome, what is your name? \n");
     scanf("%s", name);
-    printf("Hello %s.\n", name);
+    printf("Hello %s what is your favourite food?\n", name);
+    scanf("%s", food);
+    printf("%s's favourite food is %s.\n", name, food);
     return 0;
 }
