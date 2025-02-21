38a553e912fb0fb6c131b6824ffd80020e7b04ee
Elizabeth150-9 <elizabeth.gutierrez@ucas-edu.net>
2025-02-21 19:20:18 +0000

    dfs

--- functions/update_hello_world.py
+++ functions/update_hello_world.py
@@ -1,14 +1,14 @@
 # Elizabeth Gutierrez, Update hello world python
 
-name = input("What is your name?")
+name = input("What is your name?\n")
 print("Hello,", name)
 
-def values(type):
-names = input("Give me five name. \n")
-print("")
+def hello(name):
+  return f"Hello {name}"
+print(hello(input("Give me a name.")))
+print(hello(input("Give me another name.")))
+print(hello(input("Give me another name.")))
+print(hello(input("Give me another name.")))
+print(hello(input("Give me one last name.")))
 
-name = values("name")
 
-
-
-  
