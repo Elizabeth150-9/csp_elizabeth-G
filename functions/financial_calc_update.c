52b8858a3ad54be2760c341ef28d7a3e2af17d97
Elizabeth150-9 <elizabeth.gutierrez@ucas-edu.net>
2025-02-22 06:36:55 +0000

    dkj

--- functions/financial_calc_update.c
+++ functions/financial_calc_update.c
@@ -1,39 +1,35 @@
 // Elizabeth Gutierrez, Financial Calculator C update
 #include <stdio.h>
 
-float income;
-float rent;
-float utilities;
-float groceries;
-float transportation;
-float savings;
-float spendings;
+double values(char* type) {
+  double amount;
+  printf("How much is your %s: \n", type);
+  scanf("%lf", &amount);
+  return amount;
+}
 
-int main(void){
-   printf("Welcome to my financial calculator that will help you manage your monthly finances! \n");
-   printf("How much is your monthly income: \n");
-   scanf("%f", &income);
-   printf("How much is your monthly rent?\n");
-   scanf("%f", &rent);
-   printf("How much are your utilities monthly?\n");
-   scanf("%f", &utilities);
-   printf("How much are you groceries monthly?\n");
-   scanf("%f", &groceries);
-   printf("How much is your transportation?\n");
-   scanf("%f", &transportation);
-   float savings = income*.1;
-   float spendings = income-savings-rent-utilities-groceries-transportation;
-   float percent_rent = rent/income *100;
-   float percent_utilities = utilities/income *100;
-   float percent_groceries = groceries/income *100;
-   float percent_transportation = transportation/income *100;
-   float percent_spendings = spendings/income *100;
-   float percent_savings = savings/income *100;
-   printf("Your rent costs %.2f which is %.2f percent of your income\n", rent, percent_rent);
-   printf("Your utilities cost %.2f which is %.2f percent of your income\n", utilities, percent_utilities);
-   printf("Your groceries cost %.2f which is %.2f percent of your income\n", groceries, percent_groceries);
-   printf("Your transportation costs %.2f which is %.2f percent of your income\n", transportation, percent_transportation);
-   printf("Your savings are %.2f which is %.2f percent of your income\n", savings, percent_savings);
-   printf("Your money left to spend is %.2f which is %.2f percent of your income\n", spendings, percent_spendings);
+void info(double cost, double income, char* type) {
+  double percent = (cost/income) *100;
+  printf("Your %s is $%.2f which is %.2f%% of your income.\n", type, cost, percent);
+}
+
+int main() {
+  double income = values("income");
+  double rent = values("rent");
+  double utilities = values("utilities");
+  double groceries = values("groceries");
+  double transportation = values("transportation");
+
+  double savings = income *0.1;
+  double spending = income-rent-utilities-groceries-transportation-savings;
+
+  info(rent, income, "rent");
+  info(utilities, income, "utilities");
+  info(groceries, income, "groceries");
+  info(transportation, income, "transportation");
+  info(savings, income, "savings");
+  info(spending, income, "spending");
+
   return 0;
 }
+
--- functions/financial_calc_update.py
+++ functions/financial_calc_update.py
@@ -1,25 +1,18 @@
 # Elizabeth Gutierrez, Financial Calculator python update
+def values(type):
+    return input(f"How much is your {type}:\n")
+
 def info(cost, income, type):
     percent = cost/income *100
-    print(f"Your {type} is ${cost:.2f} which is {percent}% of your income.")
-
-def questions(total):
+    print(f"Your {type} is ${cost:.2f} which is {percent:.2f}% of your income.")
     
+income = values("income")
+rent = values("rent")
+utilities = values("utilities")
+groceries = values("groceries")
+transportation = values("transportation")
 
-# print statement that welcomes user and tells what the program does
-print("Welcome to my financial calculator that will help you manage your monthly finances!:")
-# ask what their income is (variable an input)
-income = float(input("What is your income\n"))
-# ask what their rent is (variable an input)
-rent = float(input("How much is your rent\n"))
-# ask what their utilities is (variable an input)
-utilities = float(input("How much are your utilities\n"))
-# ask what their groceries is (variable an input)
-groceries = float(input("How much are your groceries\n"))
-# ask what their transportation is (variable an input)
-transportation = float(input("How much is your transportation\n"))
-
-savings = income*.1
+savings = income *1
 spending = income-rent-utilities-groceries-transportation-savings
 
 info(rent, income, "rent")
