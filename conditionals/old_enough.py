b9f14643659bd9d7b3043faf9eb252a968c21c59
Elizabeth.105 <elizabeth.gutierrez@ucas-edu.net>
2025-02-26 11:48:37 -0700

    asl

--- conditionals/old_enough.py
+++ conditionals/old_enough.py
@@ -1,24 +1,32 @@
 # Elizabeth Gutierrez, Old Enough in Python
 vote = int(input("How old are you?:\n"))
-if vote > 18:
+if vote >= 18:
     print("You can vote!")
-elif vote < 18:
+elif vote <= 18:
     print("You cannot vote.")
+else:
+    print(f"You {vote}.")
 
 drive = int(input("How old are you?:\n"))
-if drive > 16:
-    print("You can drive!")
-elif drive < 16:
-    print("You cannot drive.")
+if drive >= 16:
+    print("You can drive!\n")
+elif drive <= 16:
+    print("You cannot drive.\n")
+else:
+    print(f"You {drive}.")
 
-learners_permit = int(input("How old are you?:\n"))
-if learners_permit > 15:
-    print("You can get your learners permit!")
-elif learners_permit < 15:
-    print("You cannot get your learners permit.")
+permit = int(input("How old are you?:\n"))
+if permit >= 15:
+    print("You can get your learners permit!\n")
+elif permit <= 15:
+    print("You cannot get your learners permit.\n")
+else:
+    print(f"You {permit}.")
 
 school = int(input("How old are you?:\n"))
-if school > 4:
-    print("You can go to school!")
-elif school < 4:
-    print("You cannot go to school.")
+if school >= 4:
+    print("You can go to school!\n")
+elif school <= 4:
+    print("You cannot go to school.\n")
+else:
+    print(f"You {school}.")
