4a1287f160f3bf54d3e5a027e01f882e22e271d5
Elizabeth150-9 <elizabeth.gutierrez@ucas-edu.net>
2025-03-01 05:22:51 +0000

    afg

--- conditionals/time_of_day.py
+++ conditionals/time_of_day.py
@@ -1,2 +1,26 @@
 # Elizabeth Gutierrez, Time of day in Python
-welcome = ()
+
+import time
+
+num1 = "Goodmorning"
+num2 = "Good afternoon"
+num3 = "Good evening"
+i=int(time.time())+60  # setting a checkpoint 60s in the future
+if num1 < num2:
+    print("Good morning!")
+    print("Good afternoon!")
+    print("Good evening!")
+    
+def time_to_min(hour, minute):
+    return hour*60+minute
+
+timenow = time.localtime()
+
+morning = timenow.tm_hour
+afternoon = timenow.tm_hour
+evening = timenow.tm_hour
+
+if evening >= morning and evening <= afternoon:
+    print("Good morning!\n")
+elif:
+    print("Good morning!\n") 
