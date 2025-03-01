36ed2246634b4726fb617686be095fdcd93fc749
Elizabeth150-9 <elizabeth.gutierrez@ucas-edu.net>
2025-03-01 21:33:59 +0000

    asl

--- conditionals/time_of_day.c
+++ conditionals/time_of_day.c
@@ -1,3 +1,24 @@
 // Elizabeth Gutierrez, Time of Day in C
 #include <stdio.h>
 #include <time.h>
+
+int main() {
+    time_t t;
+    struct tm *tm_info;
+
+    time(&t);
+    tm_info = localtime(&t);
+
+    printf("%d\n", tm_info->tm_hour);
+
+    if (tm_info->tm_hour <= 12) {
+        printf("Good morning!\n");
+    } else if (tm_info->tm_hour <= 18) {
+        printf("Good afternoon!\n");
+    } else {
+        printf("Good evening!\n");
+    }
+
+    return 0;
+}
+
--- conditionals/time_of_day.py
+++ conditionals/time_of_day.py
@@ -1,26 +1,15 @@
 # Elizabeth Gutierrez, Time of day in Python
 
-import time
+import datetime
 
-num1 = "Goodmorning"
-num2 = "Good afternoon"
-num3 = "Good evening"
-i=int(time.time())+60  # setting a checkpoint 60s in the future
-if num1 < num2:
+currenttime = datetime.datetime.now()
+
+print(currenttime.hour)
+
+if currenttime.hour <= 12:
     print("Good morning!")
+elif currenttime.hour <= 18:
     print("Good afternoon!")
+else:
     print("Good evening!")
-    
-def time_to_min(hour, minute):
-    return hour*60+minute
 
-timenow = time.localtime()
-
-morning = timenow.tm_hour
-afternoon = timenow.tm_hour
-evening = timenow.tm_hour
-
-if evening >= morning and evening <= afternoon:
-    print("Good morning!\n")
-elif:
-    print("Good morning!\n") 
