557cc994b1fad1f37182035eebc79b34f40c81c3
Elizabeth150-9 <elizabeth.gutierrez@ucas-edu.net>
2025-03-09 00:42:43 +0000

    asl

--- loops/fizzbuzz.c
+++ loops/fizzbuzz.c
@@ -3,17 +3,19 @@
 
 int main() {
     int x = 0;
-    while (x < 51) {
-        x = x + 1;
-        if (x % 3 == 0 && x % 5 == 0) {
-            printf("fizzbuzz\n");
-        } else if (x % 3 == 0) {
-            printf("fizz\n");
-        } else if (x % 5 == 0) {
-            printf("buzz\n");
-        } else {
-            printf("%d\n", x);
-        }
+while (x < 51) {
+    x = x + 1;
+if (x % 3 == 0 && x % 5 == 0) {
+    printf("fizzbuzz\n");
+} 
+else if (x % 3 == 0) {
+    printf("fizz\n");
+} 
+else if (x % 5 == 0) {
+    printf("buzz\n");
+}else{
+    printf("%d\n", x);
+}
     }
     return 0;
 }
