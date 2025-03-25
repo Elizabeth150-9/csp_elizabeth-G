89fff5b4ad6e6c1c196909295313f05c6d227df6
Elizabeth150-9 <elizabeth.gutierrez@ucas-edu.net>
2025-03-25 17:48:31 +0000

    expression

--- strings/silly_sentences.py
+++ strings/silly_sentences.py
@@ -1,9 +1,29 @@
 # Elizabeth Gutierrez, silly sentences python
  
-print("Welcome to my silly sentence in python program: ")
+#print("Welcome to my silly sentence in python program: ")
 
-name = (input("Enter a name: \n"))
-verb = (input("Enter a verb: \n"))
-noun = (input("Enter a noun: \n"))
+#name = (input("Enter a name: \n"))
+#verb = (input("Enter a verb: \n"))
+#noun = (input("Enter a noun: \n"))
 
-print(name, "likes to", verb, "with the", noun)
+#print(name, "likes to", verb, "with the", noun)
+
+
+
+puzzle = input("To pick 1 a word scramble or riddle,  insert 3 or 4.\n")
+third = ("imklerci\n")
+fourth = input("Maria has five children. Jack, Logan, Sarah, and Lipton. What is the name of the fifth child?\n")
+if puzzle == 3:
+    print("imklerci\n")
+elif third == "Limerick":
+    print("Correct, you won black eyes!\n")
+else:
+    print("Incorrect, try again...\n")
+if puzzle == 4:
+    print("Maria has five children. Jack, Logan, Sarah, and Lipton. What is the name of the fifth child?\n")
+elif fourth == "What":
+    print("Correct, you won green eyes!\n")
+else:
+    print("Incorrect, try again...\n")
+iyes = int(input("You have black eyes!\n"))
+eyes = int(input("You have green eyes!\n"))
