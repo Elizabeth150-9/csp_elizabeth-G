00bae9cb32d5e75599a22839eccc933d7183f669
Elizabeth150-9 <elizabeth.gutierrez@ucas-edu.net>
2025-03-27 17:49:11 +0000

    ask

--- strings/silly_sentences.py
+++ strings/silly_sentences.py
@@ -10,45 +10,38 @@
 
 
 
-#puzzle = input("To pick 1, a word scramble or riddle,  insert 3 or 4.\n")
-#third = ("Limerick")
-#fourth = ("What")
-#option_3 = input("")
-#option_4 = input("")
-#if option_3 == 3:
-#    print("imklerci\n")
-#elif option_3 == "Limerick":
-#    print("Correct, you won black eyes!\n")
-#else:
-#    print("Incorrect, try again.\n")
-#if option_4 == 4:
-#    print("Maria has five children. Jack, Jacob, Joane, and Jane. What is the name of the fifth child?\n")
-#elif option_4 == "What":
-#    print("Correct, you won green eyes!\n")
-#else:
-#    print("Incorrect, try again.\n")
-
-
-
 
 while True:    
     puzzle = input("To pick 1 a word scramble or riddle, insert 3 or 4.\n")    
     if puzzle == "3":   
         answer = input("imekilcr\n")   
         if answer.lower() == "limerick":
-            print("Correct, you won black eyes!\n") 
+            print("You won black eyes!\n") 
             break  
         else:
             print("Incorrect, try again...\n")
     elif puzzle == "4":  
         answer = input("Maria has five children. Jack, Joane, Jonah, and Jane. What is the name of the fifth child?\n")    
         if answer.lower() == "what":    
-            print("Correct, you won green eyes!\n")    
+            print("You won green eyes!\n")    
             break   
         else:    
             print("Incorrect, try again...\n")    
     else:  
         print("To pick 1 a word scramble or riddle, insert 3 or 4.\n")  
 
+puzzles = ["Black eyes", "granny white hair", "big blue ball gown with elegant puffed sleeves", "purple heelys"]
+for puzzle in puzzles:
+    print("Your character has", puzzle)
 
+prizes = ["Green eyes", "light up neon circus afro hair", "jean overalls with embroidered flowers on the pockets", "pink bedazzeled justice glow up sneakers"]
+for prize in prizes:
+    print("Your character has", prize)
 
+wins = ["Black", "light up neon circus afro hair", "big blue ball gown with elegant puffed sleeves", "pink bedazzled justics glow up sneakers"]
+for win in wins:
+    print("Your character has", win)
+
+selects = ["Green eyes", "granny white hair", "jean overalls with embroidered flowers on the pockets", "purple heelys"]
+for select in selects:
+    print("Your character has", select)
