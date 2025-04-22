1eac57b7dda21bf99759e10e3183645b833fbb8f
BETEL.net1 <elizabeth.gutierrez@ucas-edu.net>
2025-04-22 11:21:09 -0600

    asd

---
+++ javascript/notex.js
@@ -0,0 +1,7 @@
+function hello(){
+    document.getElementById("title").innerHTML = "Hello World!"
+}
+function hover(){
+    document.getElementById
+    ("img").src = "https://t4.ftcdn.net/jpg/11/55/89/89/360_F_1155898967_OroAp8ja1bGnN5PqE1usF6oIKfWWDRcS.jpg"
+}
