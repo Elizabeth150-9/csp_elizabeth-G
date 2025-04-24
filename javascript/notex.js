0dc098c389c80e59583923ffc6cd6d1311fd04bc
Elizabeth150-9 <elizabeth.gutierrez@ucas-edu.net>
2025-04-24 04:50:49 +0000

    asd

--- javascript/notex.js
+++ javascript/notex.js
@@ -1,32 +1,43 @@
-let images = ["https://t4.ftcdn.net/jpg/11/55/89/89/360_F_1155898967_OroAp8ja1bGnN5PqE1usF6oIKfWWDRcS.jpg", "https://t4.ftcdn.net/jpg/11/55/89/89/360_F_1155898967_OroAp8ja1bGnN5PqE1usF6oIKfWWDRcS.jpg", "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cat_November_2010-1a.jpg/1200px-Cat_November_2010-1a.jpg"]
-let counter = 0
+// JavaScript - JS
 
-function change(){
-    if (counter < images.length){
-        document.getElementById("img").
-        src = images[counter] 
-        counter += 1
-    }else{
-        counter = 0
-        document.getElementById("img").src = images[counter]
-    }
+function show(){
+    document.getElementById("patric").style.display = "block"
 }
 
 function hello(){
-    let name = window.prompt("What is your name?", "Koro Sensei")
+    let name = prompt("What is your name?", "Koro Sensei")
     document.getElementById("title").innerHTML = "Hello" + name + "!"
+        
 }
-function hover(){
-    document.getElementById
-    ("img").src = "https://t4.ftcdn.net/jpg/11/55/89/89/360_F_1155898967_OroAp8ja1bGnN5PqE1usF6oIKfWWDRcS.jpg"
+        
+function enter(){
+    document.getElementById("btn").style.color = "white"
+    document.getElementById("btn").style.backgroundColor = "green"
 }
-function leave(){
-    document.getElementById
-    ("img").src = "https://t4.ftcdn.net/jpg/11/55/89/89/360_F_1155898967_OroAp8ja1bGnN5PqE1usF6oIKfWWDRcS.jpg"
+function down(){
+    document.getElementById("btn").style.backgroundColor = "red"
 }
-function pop(){
-    window.alert("Really don't click that!")
+function out(){
+    document.getElementById("btn").style.backgroundColor = "black"
+    document.getElementById("btn").style.backgroundColor = "lightgrey"
 }
-function show(){
-    document.getElementById("Lost").style.display = "block"
+let images = ["https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cat_November_2010-1a.jpg/1200px-Cat_November_2010-1a.jpg", "https://media.tenor.com/-_bYOZ_6NZcAAAAM/shrek.gif"]
+let counter = 0
+
+function change(){
+    if(counter < images.length){
+        document.getElementById("image").src = images
+        [counter]
+        counter += 1
+    }else{
+        counter = 0
+        document.getElementById("image").src = images
+        [counter]
+    }
+
 }
+
+function popup(){
+    window.prompt("Don't click this. Really!")
+}
+    
