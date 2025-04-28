4d4ac62d6a6e886e3aefd8ef93c72d9c62b23114
BETEL.net1 <elizabeth.gutierrez@ucas-edu.net>
2025-04-28 11:49:27 -0600

    asd

--- javascript/hidden_image.js
+++ javascript/hidden_image.js
@@ -1,3 +1,8 @@
-function change(){
-    document.getElementById("hidden").style.display ="block"
+function appear(){
+    const appear=document.getElementById("hidden")
+    if(appear.style.display=="none"){
+        appear.style.display="appear"
+    }else{
+    appear.style.display="none"
+}
 }
