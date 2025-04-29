bb54d72a84709ee4a61091c3c38b82585826df70
Elizabeth150-9 <elizabeth.gutierrez@ucas-edu.net>
2025-04-29 23:42:44 +0000

    htf

---
+++ html/first_web_page.js
@@ -0,0 +1,8 @@
+function view(){
+    if(document.getElementById("spotify").style.display = "show"){
+    document.getElementById("spotify").style.display = "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/ea/e0/66/eae0669b-8c6b-45cc-d1da-32c7dd5ecf50/AppIcon-0-0-1x_U007emarketing-0-7-0-0-85-220.png/1200x630wa.png"
+    document.getElementById("shw").style.display = "btn"
+}else{
+    document.getElementById("hide").style.display = "none"
+}
+}
--- javascript/hidden_image.js
+++ javascript/hidden_image.js
@@ -1,3 +1,10 @@
-function change(){
-    document.getElementById("hidden").style.display ="block"
+function view(){
+    const img = document.getElementById("food_color");    
+    img.src = "https://handletheheat.com/wp-content/uploads/2013/04/red-velvet-cake-recipe-SQUARE.jpg";
+    document.getElementById("img_src").innerHTML="handelttheheat";
+}
+function resetimage(){
+    const img = document.getElementById("food_color");    
+    img.src = "https://chefmaster.com/cdn/shop/files/NaturalRedLiqua-GelLiquidFoodColoring7oz.png?v=1699265942";
+    document.getElementById("img_src").innerHTML="chefmaster";
 }
