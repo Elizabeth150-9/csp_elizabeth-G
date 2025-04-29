9ff53908eb6c01cd0a94b4b7ad468a44dc69b868
BETEL.net1 <elizabeth.gutierrez@ucas-edu.net>
2025-04-29 13:30:37 -0600

    asd

--- javascript/hidden_image.js
+++ javascript/hidden_image.js
@@ -1,8 +1,10 @@
-function appear(){
-    const appear=document.getElementById("hidden")
-    if(appear.style.display=="none"){
-        appear.style.display="appear"
-    }else{
-    appear.style.display="none"
+function view(){
+    const img = document.getElementById("food_color");    
+    img.src = "https://handletheheat.com/wp-content/uploads/2013/04/red-velvet-cake-recipe-SQUARE.jpg";
+    document.getElementById("img_src").innerHTML="handelttheheat";
 }
+function resetimage(){
+    const img = document.getElementById("food_color");    
+    img.src = "https://chefmaster.com/cdn/shop/files/NaturalRedLiqua-GelLiquidFoodColoring7oz.png?v=1699265942";
+    document.getElementById("img_src").innerHTML="chefmaster";
 }
