e7b3fb5c0976a03c767fa25f5e4066e09036141e
Elizabeth150-9 <elizabeth.gutierrez@ucas-edu.net>
2025-04-30 18:12:04 +0000

    asd

--- javascript/hidden_image.js
+++ javascript/hidden_image.js
@@ -1,10 +1,5 @@
 function view(){
-    const img = document.getElementById("food_color");    
+    const img = document.getElementById("pop");    
     img.src = "https://handletheheat.com/wp-content/uploads/2013/04/red-velvet-cake-recipe-SQUARE.jpg";
-    document.getElementById("img_src").innerHTML="handelttheheat";
-}
-function resetimage(){
-    const img = document.getElementById("food_color");    
-    img.src = "https://chefmaster.com/cdn/shop/files/NaturalRedLiqua-GelLiquidFoodColoring7oz.png?v=1699265942";
-    document.getElementById("img_src").innerHTML="chefmaster";
+    document.getElementById("pop").innerHTML="chefmaster";
 }
