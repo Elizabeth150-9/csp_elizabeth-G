fc28813a0d936ef4d95557872b75a84bee18211e
BETEL.net1 <elizabeth.gutierrez@ucas-edu.net>
2025-05-01 13:25:23 -0600

    asd

--- html/first_web_page.js
+++ html/first_web_page.js
@@ -1,19 +1,18 @@
-function view(){
-    const img = document.getElementById("walks");    
-    img.src = "https://upload.wikimedia.org/wikipedia/commons/b/b3/Upper_Provo_River_Utah.jpg";
-    document.getElementById("img_src").innerHTML="wikimedia";
-}
-
 function resetimage(){
     const img = document.getElementById("walks");    
     img.src = "https://swiftmedia.s3.amazonaws.com/mountain.swiftcom.com/images/sites/2/2023/01/17233033/Screenshot-2023-01-21-at-4.44.44-PM-1024x687.png";
     document.getElementById("img_src").innerHTML="swiftmedia";
 }
-
 function view(){
-    if(document.getElementById("img").style.display === "block"){
-        document.getElementById("img").style.display = "none"
+    const img = document.getElementById("walks");    
+    img.src = "https://upload.wikimedia.org/wikipedia/commons/b/b3/Upper_Provo_River_Utah.jpg";
+    document.getElementById("img_src").innerHTML="wikimedia";
+}
+function see(){
+    img.src = "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/ea/e0/66/eae0669b-8c6b-45cc-d1da-32c7dd5ecf50/AppIcon-0-0-1x_U007emarketing-0-7-0-0-85-220.png/1200x630wa.png"
+    if(document.getElementById("button").style.display === "see"){
+        document.getElementById("button").style.display = "none"
     }else{
-        document.getElementById("img").style.display = "block"
+        document.getElementById("button").style.display = "see"
     } 
 }
--- javascript/hidden_image.js
+++ javascript/hidden_image.js
@@ -1,7 +1,12 @@
 function view(){
-    if(document.getElementById("img").style.display === "block"){
-        document.getElementById("img").style.display = "none"
+    const img = document.getElementById("cake");    
+    img.src = "https://upload.wikimedia.org/wikipedia/commons/b/b3/Upper_Provo_River_Utah.jpg";
+    document.getElementById("img_src").innerHTML="wikimedia";
+}
+function view(){
+    if(document.getElementById("button").style.display === "block"){
+        document.getElementById("cake").style.display = "none"
     }else{
-        document.getElementById("img").style.display = "block"
+        document.getElementById("button").style.display = "block"
     } 
 }
