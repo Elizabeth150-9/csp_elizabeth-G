64e075d884b7a87f4747ceac82dceaad07f9718a
Elizabeth150-9 <elizabeth.gutierrez@ucas-edu.net>
2025-05-03 00:05:34 +0000

    asd

--- javascript/hidden_image.js
+++ javascript/hidden_image.js
@@ -1,12 +1,7 @@
-function view(){
-    const img = document.getElementById("cake");    
-    img.src = "https://upload.wikimedia.org/wikipedia/commons/b/b3/Upper_Provo_River_Utah.jpg";
-    document.getElementById("img_src").innerHTML="wikimedia";
+function showImage() {
+    document.getElementById("Hover").style.display = "block";
 }
-function view(){
-    if(document.getElementById("button").style.display === "block"){
-        document.getElementById("cake").style.display = "none"
-    }else{
-        document.getElementById("button").style.display = "block"
-    } 
+
+function hideImage() {
+    document.getElementById("Hover").style.display = "none";
 }
