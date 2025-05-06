2650c45814016bfb72b6e05894086454252873b7
Elizabeth150-9 <elizabeth.gutierrez@ucas-edu.net>
2025-05-06 00:18:06 +0000

    recent_one

--- html/final_staycation_project.js
+++ html/final_staycation_project.js
@@ -12,3 +12,21 @@ function showBoth(){
     let content = document.getElementById("more");
     content.style.display = content.style.display === "none" ? "block" : "none";
 }
+
+
+KEEP
+
+
+function resetimage(){
+    let img = document.getElementById("snow");
+    img.onmouseover = () => img.src = "https://scene7.vailresorts.com/is/image/vailresorts/20220302_PC_Markewitz_089?wid=4000&resMode=sharp2&w=4000&h=2666&fit=constrain,1&dpr=on,2.625";
+    img.onmouseout = () => img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQa8GW-TkBiOEKKrJlRkyXwkmaLCg6da27PQg&s";
+    }
+    function showButton(){
+    const content = document.getElementById("button");
+    if (content.style.display === "none") {
+        content.style.display = "block";
+    }else{
+        content.style.display = "none";
+        }
+    }
