6f200c246306014ad473a02886f997920d8ef226
Elizabeth150-9 <elizabeth.gutierrez@ucas-edu.net>
2025-05-06 02:48:39 +0000

    final

--- html/final_staycation_project.js
+++ html/final_staycation_project.js
@@ -1,26 +1,7 @@
 function resetimage(){
-    const img = document.getElementById("snow");    
-    img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQa8GW-TkBiOEKKrJlRkyXwkmaLCg6da27PQg&s";
-    document.getElementById("img_src").innerHTML="encrypted-tbno";
-}
-function view(){
-    const img = document.getElementById("snow");
-    img.src = "https://scene7.vailresorts.com/is/image/vailresorts/20220302_PC_Markewitz_089?wid=4000&resMode=sharp2&w=4000&h=2666&fit=constrain,1&dpr=on,2.625";
-    document.getElementById("img_src").innerHTML="scene7";
-}
-function showBoth(){
-    let content = document.getElementById("more");
-    content.style.display = content.style.display === "none" ? "block" : "none";
-}
-
-
-KEEP
-
-
-function resetimage(){
     let img = document.getElementById("snow");
     img.onmouseover = () => img.src = "https://scene7.vailresorts.com/is/image/vailresorts/20220302_PC_Markewitz_089?wid=4000&resMode=sharp2&w=4000&h=2666&fit=constrain,1&dpr=on,2.625";
-    img.onmouseout = () => img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQa8GW-TkBiOEKKrJlRkyXwkmaLCg6da27PQg&s";
+    img.onmouseout = () => img.src = "https://upload.wikimedia.org/wikipedia/commons/8/84/Ski_Famille_-_Family_Ski_Holidays.jpg";
     }
     function showButton(){
     const content = document.getElementById("button");
